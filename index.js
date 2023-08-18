1817ef6e4c2c5184db7cb22dc482ed6045e0c6a7
Thomas Eitrheim <teitrheim@gmail.com>
2023-08-18 17:12:56 +0200

    added pages

---
+++ index.js
@@ -0,0 +1,3 @@
+// const WOO_API_KEY = `ck_ff96352a4abf2487595675a1e29cd83f7e08c33c`;
+// const WOO_API_SECRET = `cs_49a14fb8aadaea141dab047291d789376caf5b87`;
+// const BASE_URL = `https://localhost/Flower_Power/wp-json/wc/v2/products?consumer_key=${WOO_API_KEY}&consumer_secret=${WOO_API_SECRET}`;
